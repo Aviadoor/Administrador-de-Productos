ce3141909c235886c46d43fbb90d89125b2d0e7a
Aviadoor <diego.2003.29.12@gmail.com>
2026-04-13 08:26:15 -0500

    Agregando gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'SistemaInventario.apps.SistemaInventarioConfig',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
